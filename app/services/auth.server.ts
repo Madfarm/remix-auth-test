b62195afc21bdeda3f92bcee4a3c77e8143c9316
AnthonyScott <anthony.scott1005@gmail.com>
2024-09-10 22:27:03 -0400

    Add authentication service

---
+++ app/services/auth.server.ts
@@ -0,0 +1,52 @@
+import { Authenticator, AuthorizationError } from "remix-auth";
+import { FormStrategy } from 'remix-auth-form';
+import { sessionStorage, AuthUser } from "~/services/session.server";
+
+
+const authenticator = new Authenticator<AuthUser | Error | null>(sessionStorage, {
+    sessionKey: "sessionKey", // keep in sync
+    sessionErrorKey: "sessionErrorKey", // keep in sync
+  });
+
+
+  // Tell the Authenticator to use the form strategy
+authenticator.use(
+    new FormStrategy(async ({ form }) => {
+  
+      // get the data from the form...
+      let email = form.get('email') as string;
+      let password = form.get('password') as string;
+  
+      // initialize the user here
+      let user = null;
+  
+      // do some validation, errors are in the sessionErrorKey
+      if (!email || email?.length === 0) throw new AuthorizationError('Bad Credentials: Email is required')
+      if (typeof email !== 'string')
+        throw new AuthorizationError('Bad Credentials: Email must be a string')
+  
+      if (!password || password?.length === 0) throw new AuthorizationError('Bad Credentials: Password is required')
+      if (typeof password !== 'string')
+        throw new AuthorizationError('Bad Credentials: Password must be a string')
+  
+      // login the user, this could be whatever process you want
+      if (email === 'aaron@mail.com' && password === 'password') {
+        user = {
+          name: email,
+          token: `${password}-${new Date().getTime()}`,
+        };
+  
+        // the type of this user must match the type you pass to the Authenticator
+        // the strategy will automatically inherit the type if you instantiate
+        // directly inside the `use` method
+        return await Promise.resolve({ ...user });
+  
+      } else {
+        // if problem with user throw error AuthorizationError
+        throw new AuthorizationError("Bad Credentials")
+      }
+  
+    }),
+  );
+  
+  export default authenticator
